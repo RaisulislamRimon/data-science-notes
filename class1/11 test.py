dc1db793a66d32e29d0d5bdbf93448836439fd5f
RaisulislamRimon <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>
2024-09-30 23:32:20 +0600

    test

---
+++ class1/11 test.py
@@ -0,0 +1,21 @@
+# take two number as input
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+# add these number and store it in result
+result = int(a) + int(b)
+
+# show the result 
+print(result)
+
+# # or, 
+# # take two number as input
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# # add these number and store it in result
+# result = a + b
+
+# # show the result 
+# print(result)
+
