3f0ee4be1c9812b37f2e3a6b7cc83c345e5fddc3
RaisulislamRimon <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>
2024-09-30 22:33:10 +0600

    variable

---
+++ class1/3 type.py
@@ -0,0 +1,27 @@
+# variable declaration rules:
+#  variable name must be :
+# A-Z, a-z, 0-9
+#  must start with a letter or the underscore(_) character 
+#  must not be a reserved keyword
+#  vaiable names are case sensitive
+
+
+print(10) # print int number
+print("10") # print string number
+
+print("Num", 10) # print int number
+print("String","10") # print string number
+
+x = 10
+print(x)
+
+name = "Hello Python"
+print(name)
+
+a = 20
+print(a)
+
+# aBc = 30
+# print(abc) # it will show error:
+# # NameError: name 'abc' is not defined. Did you mean: 'aBc'? Or did you forget to import 'abc'?    
+
