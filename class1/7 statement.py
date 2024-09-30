b933e11b374c85973f7c85b209ad1beaf63a6737
RaisulislamRimon <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>
2024-09-30 23:06:14 +0600

    comment in python

---
+++ class1/7 statement.py
@@ -0,0 +1,7 @@
+# every line is a statement
+
+a = 10
+print(type(a))
+
+b = 'hello'
+print(type(b))
---
+++ class1/8 comment.py
@@ -0,0 +1,16 @@
+# single line comment
+
+"""
+Multi line comment
+"""
+
+
+a = 10 # this is comment example
+"""
+we can use 
+multiline 
+comment 
+also 
+in python"""
+
+
