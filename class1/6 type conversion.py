4c379b1ca741862976b0c201cf00e2879f48fe13
RaisulislamRimon <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>
2024-09-30 23:00:45 +0600

    type conversion

--- class1/6 type conversion.py
+++ class1/6 type conversion.py
@@ -1,10 +1,18 @@
 # 1
 
-num1 = int(float(input("enter first number ")))
-num2 = float(input("enter second number "))
+# num1 = int(float(input("enter first number ")))
+# num2 = float(input("enter second number "))
 
-sum = num1 + num2 
+# sum = num1 + num2 
 
-print(type(num1))
+# print(type(num1))
 
-print(type(sum))
+# print(type(sum))
+
+
+#  2
+num = 10
+# num = float(num) # or
+# print(type(num))
+float_num = float(num)
+print(float_num)
