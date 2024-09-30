0dd6510e167e38bf6a0283869c8d294edd4f0ab6
RaisulislamRimon <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>
2024-09-30 23:12:43 +0600

    string

---
+++ class1/9 string.py
@@ -0,0 +1,16 @@
+'''a string is a cllection of characters.
+
+can be accessed & slicesd by index '''
+
+'''0  1  2  3  4
+H  E  L  L  O 
+-5 -4 -3 -2 -1'''
+
+# A = 'HELLO WORLD'
+# print(A)
+
+a= 'hello world'
+print(a)
+print(a[6]) # w
+print(a[-5]) # w
+
