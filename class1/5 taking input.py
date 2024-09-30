acfad9afcd0c42887b0b445bdd16efe518ed3b0f
RaisulislamRimon <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>
2024-09-30 22:50:15 +0600

    update

--- class1/5 taking input.py
+++ class1/5 taking input.py
@@ -1,24 +1,33 @@
 # input()
 # we can take input by using this function
 
-a = 10
-b = 20
+# a = 10
+# b = 20
 
-sum = a + b
-print(sum)
+# sum = a + b
+# print(sum)
 
-# we are taking input using console
-name = input("Please type your name: ")
-print(name)
-print("Your name is : ", name)
-print("data type is : ", type(name))
+# # we are taking input using console
+# name = input("Please type your name: ")
+# print(name)
+# print("Your name is : ", name)
+# print("data type is : ", type(name))
 
+
+# # 3
+
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+
+# totalSum = num1 + num2
+
+# print("Total Sum = ", totalSum) # Total Sum =  1020
 
 # 3
 
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 totalSum = num1 + num2
 
-print("Total Sum = ", totalSum) # Total Sum =  1020
+print("Total Sum = ", totalSum) # Total Sum =  30
