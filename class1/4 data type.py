a94e2b870f909eae31f387fde9c187eb85319dda
RaisulislamRimon <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>
2024-09-30 22:41:33 +0600

    data type

---
+++ class1/4 data type.py
@@ -0,0 +1,31 @@
+# python basic data types:
+
+# str
+
+# int, float, complex
+
+# list, typle, range
+
+# dict
+
+# bool(boolean type- true, false)
+
+aBc = 10
+print(type(aBc)) # <class 'int'>
+print(type("20")) # <class 'str'>
+print(type(True)) # <class 'bool'>
+
+# Print(type(True)) # NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+print(type(1+2j)) # <class 'complex'>
+
+bCd = "Hello"
+print(type("10"))
+print(type("hello"))
+print(type(bCd))
+
+hi = "hello"
+Hi = "world"
+print(hi)
+print(Hi)
+print(type("hi"))
