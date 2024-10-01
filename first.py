d6ea8d34257ae28eca4f203e6c0befc30e3dacb7
RaisulislamRimon <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>
2024-10-02 00:19:26 +0600

    update

--- first.py
+++ first.py
@@ -25,3 +25,29 @@
 # print(x, y)
 
 
+# a = 10
+# print(type(a))
+
+# name = 'rimon'
+# print(type(name))
+
+# num1 = 10
+# num2 = 20
+# sum = num1 + num2
+# print(sum)
+
+# num1 = input('enter first number ')
+# num2 = input('enter second number ')
+# sum = num1 + num2
+# print(sum)
+# print(type(sum))
+
+# num1 = int(input('enter first number '))
+# num2 = int(input('enter second number '))
+# sum = num1 + num2
+# print(sum)
+# print(type(sum))
+
+name = input("enter your name: ")
+print("Your name is: ", name, ".")
+print(type(name))
