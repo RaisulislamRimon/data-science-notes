883ad97ec1e7504705b986e023555e0d0f0ef66a
RaisulislamRimon <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>
2024-10-01 23:50:26 +0600

    update

--- first.py
+++ first.py
@@ -1,4 +1,27 @@
-print("hello world")
-print(10)
-print(10)
-print(10)
+# print("hello world")
+# print(10)
+# print(10)
+# print(10)
+
+# x = 10
+# print(x)
+
+# y = 20
+# print(y)
+
+# y = 20
+# print("y")
+
+# print(z)
+
+
+# y = 20
+# y = 30
+# print(y)
+
+
+# x = 10
+# y = 20
+# print(x, y)
+
+
