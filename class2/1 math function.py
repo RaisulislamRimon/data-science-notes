964872b173dc4ede8d206489de6d41179ec8be78
rimonppip0 <rimonppip0@gmail.com>
2024-10-04 19:05:08 +0000

    round number check

---
+++ class2/1 math function.py
@@ -0,0 +1,12 @@
+# print("hello world!")
+
+# num1 = 10
+# print(type(num1))
+# print(num1)
+
+# number = 10.5
+# print(number)
+# print(type(number))
+
+num1 = 10.5
+print("round number: ", round(num1))
