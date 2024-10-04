bdb4f207eca7e4ec7787c872aad020113bfddd6f
rimonppip0 <rimonppip0@gmail.com>
2024-10-04 19:43:14 +0000

    condition

---
+++ class2/3 condition.py
@@ -0,0 +1,17 @@
+# print("program 1")
+# x = 30
+# if x > 20:
+#     print('x is greater')
+
+# print('I am outside of any condition')
+
+
+# print("program 2")
+# y = 10
+# if y > 20:
+#     print("y is greater")
+
+# print("I am outside of any condition")
+
+
+print("program 3")
