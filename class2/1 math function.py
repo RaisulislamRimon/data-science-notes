7eea252daadcc4a071a2cd4f767ff034f0acac4c
rimonppip0 <rimonppip0@gmail.com>
2024-10-04 19:14:35 +0000

    math ceil & floor function

--- class2/1 math function.py
+++ class2/1 math function.py
@@ -11,5 +11,9 @@
 # num1 = 10.5
 # print("round number: ", round(num1))
 
-num1 = -10.5
-print("absolute number", abs(num1))
+# num1 = -10.5
+# print("absolute number", abs(num1))
+
+import math
+print(math.ceil(3.9))
+print(math.floor(3.9))
