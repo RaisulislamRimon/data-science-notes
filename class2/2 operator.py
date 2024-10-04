1b02d401a6be7e680ae14375140ab1949d08a022
rimonppip0 <rimonppip0@gmail.com>
2024-10-04 19:37:01 +0000

    operator

---
+++ class2/2 operator.py
@@ -0,0 +1,17 @@
+# x = 5
+# y = 10
+# print( x > y)
+# print(x < y)
+# print(x == y)
+
+# x = 10
+# y = 20
+# if x > y:
+#     print("x is greater than y")
+# else:
+#     print("y is greater than x")
+
+x = 5
+y = 5
+print(x > 0 and y > 0)
+
