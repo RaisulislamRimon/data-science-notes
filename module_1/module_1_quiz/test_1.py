710ef86df450286aa36e4515e2e84a09ea92c960
RaisulislamRimon <rimonppip0@gmail.com>
2024-10-05 17:08:18 +0600

    test programs

---
+++ module_1/module_1_quiz/test_1.py
@@ -0,0 +1,22 @@
+# # test 1
+# var = "James" * 2 * 3
+# print(var)
+
+# #  test 2
+# x = 10
+# y = 50
+
+# if x ** 2 > 100 and y < 100:
+ 	
+
+
+# # test 3
+# print(2 % 6)
+
+
+# # test 4
+# print(10 - 4 * 2)
+
+
+# # test 5
+# print(2**2**2)
