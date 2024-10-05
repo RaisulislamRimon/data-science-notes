75b547b3d123bc895ab0d4d8d6c3212cd9cb6187
rimonppip0 <rimonppip0@gmail.com>
2024-10-05 07:01:52 +0000

    if elif else condition

--- class2/3 condition.py
+++ class2/3 condition.py
@@ -14,11 +14,27 @@
 # print("I am outside of any condition")
 
 
-print("program 3")
-# mark = 34
-mark = float(input("Enter students mark: "))
+# print("program 3")
+# # mark = 34
+# mark = float(input("Enter students mark: "))
 
-if mark >= 33:
-    print("pass")
+# if mark >= 33:
+#     print("pass")
+# else:
+#     print("fail")
+
+
+print("program 4")
+num1 = 10
+num2 = 20
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than f{num2}")
+elif num2 > num1:
+    print(f"{num2} is greater than f{num1}")
+elif num1 == num2:
+    print("both numbers are equal.")
 else:
-    print("fail")
+    print("something went wrong.")
