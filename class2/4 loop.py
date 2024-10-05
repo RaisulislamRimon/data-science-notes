1ce9487c99c55a974ae5659c7d56a50509ac6cb6
rimonppip0 <rimonppip0@gmail.com>
2024-10-05 07:06:53 +0000

    while-endless loop

--- class2/4 loop.py
+++ class2/4 loop.py
@@ -1,21 +1,30 @@
-## program 1
-# Initialize the counter variable to 1
-counter = 1 
-# Print the current value of the counter
-print("Counter is: ", counter)  
-# Increment the counter by 1
-counter = counter + 1  
-# Print the current value of the counter
-print("Counter is: ", counter)  
-# Increment the counter by 1
-counter = counter + 1  
-# Print the current value of the counter
-print("Counter is: ", counter)  
-# Increment the counter by 1
-counter = counter + 1  
-# Print the current value of the counter
-print("Counter is: ", counter)  
-# Increment the counter by 1
-counter = counter + 1  
-# Print the current value of the counter
-print("Counter is: ", counter)
+# ## program 1
+# # Initialize the counter variable to 1
+# counter = 1 
+# # Print the current value of the counter
+# print("Counter is: ", counter)  
+# # Increment the counter by 1
+# counter = counter + 1  
+# # Print the current value of the counter
+# print("Counter is: ", counter)  
+# # Increment the counter by 1
+# counter = counter + 1  
+# # Print the current value of the counter
+# print("Counter is: ", counter)  
+# # Increment the counter by 1
+# counter = counter + 1  
+# # Print the current value of the counter
+# print("Counter is: ", counter)  
+# # Increment the counter by 1
+# counter = counter + 1  
+# # Print the current value of the counter
+# print("Counter is: ", counter)
+
+
+# program 2 - endless loop
+counter = 1
+
+while counter <= 5:
+    print("Counter is ", counter)
+
+print("Done")
