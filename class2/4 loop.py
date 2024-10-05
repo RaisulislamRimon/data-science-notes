f1dac0fdb43b0d6574246be70f5937c3dc0c0fc2
rimonppip0 <rimonppip0@gmail.com>
2024-10-05 07:05:23 +0000

    basic loop

---
+++ class2/4 loop.py
@@ -0,0 +1,21 @@
+## program 1
+# Initialize the counter variable to 1
+counter = 1 
+# Print the current value of the counter
+print("Counter is: ", counter)  
+# Increment the counter by 1
+counter = counter + 1  
+# Print the current value of the counter
+print("Counter is: ", counter)  
+# Increment the counter by 1
+counter = counter + 1  
+# Print the current value of the counter
+print("Counter is: ", counter)  
+# Increment the counter by 1
+counter = counter + 1  
+# Print the current value of the counter
+print("Counter is: ", counter)  
+# Increment the counter by 1
+counter = counter + 1  
+# Print the current value of the counter
+print("Counter is: ", counter)
