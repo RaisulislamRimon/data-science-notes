9acbb9e2126952efb7459016ba922484d4ad6de4
rimonppip0 <rimonppip0@gmail.com>
2024-10-05 07:14:28 +0000

    finding the summation of numbers from 1 to 5

--- class2/4 loop.py
+++ class2/4 loop.py
@@ -30,11 +30,34 @@
 # print("Done")
 
 
-# program 3
-counter = 1
+# # program 3
+# counter = 1
 
-while counter <= 5:
-    print("Counter is ", counter)
-    counter = counter + 1
+# while counter <= 5:
+#     print("Counter is ", counter)
+#     counter = counter + 1
+
+# print("Done")
+
+
+# program 4
+# finding the summation of numbers from 1 to 5
+
+# Initialize the variables
+number = 1
+# Initialize the result variable to 0
+result = 0
+
+# Use a while loop to iterate from 1 to 5
+while number <= 5:
+    # Print the current number
+    print("Number is ", number)
+    # Add the current number to the result
+    result = result + number
+    # Increment the number by 1
+    number = number + 1
+
+# Print the final result
+print("Result is ", result)
 
 print("Done")
