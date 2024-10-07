f31a5cc3752fa45297b662a831fe0e2fd4703904
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:05:03 +0000

    list tuple slice program

---
+++ module_2/module_2_class/3 list tuple slice.py
@@ -0,0 +1,15 @@
+# list tuple slice program
+# list item: 0, 1, 2, 3, 4, 5
+            -5 -4  -3  -2  -1
+
+bazar_list = [
+    'chini', 
+    'lobon', 
+    'morich',
+    15,
+    3.14156,
+    True
+]
+
+print(bazar_list)
+print(bazar_list[2:5])
