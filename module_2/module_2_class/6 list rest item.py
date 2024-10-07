29bd92f494ce12b98bcc16716fd183fc5fa31989
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:16:09 +0000

    list rest item

---
+++ module_2/module_2_class/6 list rest item.py
@@ -0,0 +1,14 @@
+bazar_list = [
+    'chini', 
+    'lobon', 
+    'morich',
+    'ada'
+]
+
+first_item, *rest_of_the_item = bazar_list
+# first_item, second_item, *rest_of_the_item = bazar_list
+
+print(first_item)
+print(type(first_item))
+print(rest_of_the_item)
+print(type(rest_of_the_item))
