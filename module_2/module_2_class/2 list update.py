32a39830861a5283a9c236d4d39234f7469ec318
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 15:56:32 +0000

    update the value of a list

---
+++ module_2/module_2_class/2 list update.py
@@ -0,0 +1,20 @@
+# update the specific value of a list - program (array)
+
+bazar_list = [
+    'chini',
+    'lobon',
+    'morich',
+    25,
+    3.14156,
+    True,
+]
+
+# update the first value of the list 
+bazar_list[0] = "ada"
+print(bazar_list)
+
+bazar_list[3] = bazar_list[3] + 5
+print(bazar_list)
+
+bazar_list[2] = "lobongo"
+print(bazar_list)
