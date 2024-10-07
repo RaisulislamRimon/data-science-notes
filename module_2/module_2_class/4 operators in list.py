c8f7e843d68f1b5d28f9a72bb6929f5b3a3e6d5c
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:09:17 +0000

    combine list

---
+++ module_2/module_2_class/4 operators in list.py
@@ -0,0 +1,15 @@
+# combine the both list in one list - program
+
+bazar_list = [
+    'chini', 'lobon', 'morich'
+]
+
+bazar_list_new = [ 'peyaj', 'darchini']
+
+bazar_list_new_new = ['egg', 'beef', 'morich'] 
+
+print(bazar_list + bazar_list_new)
+
+print(bazar_list + bazar_list_new + bazar_list_new_new)
+
+print(bazar_list * 3)
