9adfebb737b2f9dca046c4efcb76f677025896eb
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 15:50:12 +0000

    list program

---
+++ module_2/module_2_class/1 list.py
@@ -0,0 +1,24 @@
+bazar_list = [
+    'chini',
+    'lobon',
+    'morich',
+    25,
+    3.14156,
+    True,
+]
+
+print(bazar_list)
+print(type(bazar_list))
+print(bazar_list[0])
+print(type(bazar_list[1]))
+print(bazar_list[3])
+print(type(bazar_list[3]))
+print(bazar_list[4])   
+print(type(bazar_list[4]))
+print(bazar_list[5])
+print(type(bazar_list[5]))
+print(bazar_list[1])
+print(type(bazar_list[1]))
+
+print(bazar_list[3] + 3) # 25 + 3 = 28
+
