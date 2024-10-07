5712a37da594eb8b4d2b51eb1d83858ee7b7628a
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:31:18 +0000

    list extend

---
+++ module_2/module_2_class/11 list entend.py
@@ -0,0 +1,14 @@
+bazar_list = [
+    'lebu',
+    'chini',
+    'ada'
+]
+
+bazar_list_new = [
+    'daruchini',
+    'morich'
+]
+
+bazar_list.extend(bazar_list_new)
+print(bazar_list)
+print(bazar_list_new)
