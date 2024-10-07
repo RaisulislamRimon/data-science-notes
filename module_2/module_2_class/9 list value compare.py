dd3c80c2043864a2c8c6df8c0878354eec00ae85
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:27:11 +0000

    list value compare

---
+++ module_2/module_2_class/9 list value compare.py
@@ -0,0 +1,19 @@
+# rules of comparing list 
+"""
+it will check first value of both list 
+if first item is greater than second item 
+then it will return true
+if first item is lower than second item
+then it will return false 
+if first item is equal to second item
+then it will check next item of both list
+"""
+
+
+list1 = [5, 12, 18, 21, 51]
+list2 = [3, 21, 15]
+
+if list1 > list2:
+    print("list 1 is greater")
+else:
+    print("list 2 is greater")
