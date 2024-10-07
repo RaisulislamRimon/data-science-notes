9927cf17ba49c06399f4c6dad04f28ff1ce727a5
rimonppip0 <rimonppip0@gmail.com>
2024-10-07 16:13:22 +0000

    list unpack

---
+++ module_2/module_2_class/5 list unpack.py
@@ -0,0 +1,13 @@
+bazar_list = [
+    'chini', 
+    'lobon', 
+    'morich'
+]
+
+item1, item2 ,item3 = bazar_list
+# item1, item2 ,item3, items4 = bazar_list
+
+print(item1)
+print(item2)
+print(item3)
+# print(item4)
