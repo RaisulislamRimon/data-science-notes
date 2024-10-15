fed788e4917f24715ae4c591557d851d29671f7e
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 08:44:06 +0000

    remove function

--- module_2/module_2_class/class1/15 list remove.py
+++ module_2/module_2_class/class1/15 list remove.py
@@ -1,8 +1,14 @@
+# remove function will remove the item from the list
+# it will not take any index number as an arguments
+# it will take the item name as an argument
+
 bazar_list = [
 'lebu',
 'chini',
 'ada'
 ]
 
-# bazar_list.remove()
-# print(bazar_list)
+print('bazar_list: ', bazar_list)
+
+bazar_list.remove('chini')
+print('bazar_list after using remove function: ', bazar_list)
