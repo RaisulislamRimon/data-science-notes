7c001d676a9f82d4f275b5ce4f3fdb8abed2671f
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:03:41 +0000

    tuple

---
+++ module_2/module_2_class/class1/21 tuple.py
@@ -0,0 +1,9 @@
+my_tuple = (1, 2, 3, "apple", "banana")
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[3])
+print(type(my_tuple))
+
+bazar_list = ('chini', 'lobon', 'ada', True, 1, 0, 3)
+print(bazar_list)
+print(type(bazar_list))
