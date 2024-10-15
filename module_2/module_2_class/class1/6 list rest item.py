5ec2eff3132af21afed6993fa5d5ebd4230f7948
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 06:47:59 +0000

    list rest item

--- module_2/module_2_class/class1/6 list rest item.py
+++ module_2/module_2_class/class1/6 list rest item.py
@@ -12,3 +12,18 @@ print(first_item)
 print(type(first_item))
 print(rest_of_the_item)
 print(type(rest_of_the_item))
+
+player_names = list((
+    "rahim", "karim", "saddam", "rimon", "amil", "joy"
+))
+
+print('player_names:', player_names)
+print("type:", type(player_names))
+
+first_player, secondPlayer, *restOfThePlayer = player_names
+
+print(first_player)
+print(secondPlayer)
+
+print('restOfThePlayer: ', restOfThePlayer)
+print('type of restOfThePlayer: ', type(restOfThePlayer         ))
