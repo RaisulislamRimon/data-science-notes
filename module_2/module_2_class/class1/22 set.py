2cb146ce85718c2fb38555a030e562b582397991
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:11:17 +0000

    set

---
+++ module_2/module_2_class/class1/22 set.py
@@ -0,0 +1,20 @@
+# example of set
+fruits_set = {"apple", "banana", "jackfruit"}
+print('set: ', fruits_set)
+print('type of set:', type(fruits_set))
+# print a new line
+print()
+
+# example of list
+fruits_list = ["apple", "banana", "jackfruit"]
+print('list: ', fruits_list)
+print('type of list:', type(fruits_list))
+# print a new line
+print()
+
+# example of tuple
+fruits_tuple = ("apple", "banana", "jackfruit")
+print('tuple: ', fruits_tuple)
+print('type of tuple:', type(fruits_tuple))
+# print a new line
+print()
