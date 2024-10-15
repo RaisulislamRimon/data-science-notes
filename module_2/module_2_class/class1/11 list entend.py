8724967138018d5e110717d5f37e5af47b09487f
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 08:10:29 +0000

    bazar list entend

--- module_2/module_2_class/class1/11 list entend.py
+++ module_2/module_2_class/class1/11 list entend.py
@@ -9,6 +9,9 @@ bazar_list_new = [
     'morich'
 ]
 
+print('bazar_list + bazar_list_new: ', bazar_list + bazar_list_new)
+
 bazar_list.extend(bazar_list_new)
-print(bazar_list)
-print(bazar_list_new)
+print('bazar_list entend: ', bazar_list)
+
+print('bazar_list_new:', bazar_list_new)
