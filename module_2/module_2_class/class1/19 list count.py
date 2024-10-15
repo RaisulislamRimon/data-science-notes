7e7d9dca463444218d87cfef4e333e6685e0b473
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 09:21:35 +0000

    list count

---
+++ module_2/module_2_class/class1/19 list count.py
@@ -0,0 +1,33 @@
+# Create a list of numbers
+numbers = [1, 1, 2, 3, 4, 5, 1]
+
+# Count the number of times 1 appears in the list
+count_of_1 = numbers.count(1)
+print("The number 1 appears", count_of_1, "times in the list.")
+
+
+# Create a list of names
+names = ['john', 'peter', 'john', 'peter', 'jack']
+
+# Count the number of times 'john' appears in the list
+count_of_john = names.count('john')
+print("The name 'john' appears", count_of_john, "times in the list.")
+
+
+# Create a list of items from a tuple
+bazar_list = list(("chini", "ada", "lobon", "chini"))
+
+
+# Count the number of times 'chini' appears in the list
+count_of_chini = bazar_list.count("chini")
+print("The item 'chini' appears", count_of_chini, "times in the list.")
+
+
+# Count the number of times 'ada' appears in the list
+count_of_ada = bazar_list.count("ada")
+print("The item 'ada' appears", count_of_ada, "times in the list.")
+
+
+# Count the number of times 'lobon' appears in the list
+count_of_lobon = bazar_list.count("lobon")
+print("The item 'lobon' appears", count_of_lobon, "times in the list.")
