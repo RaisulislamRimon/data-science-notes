97e45503d5b271863837c63df56eaef2c117e949
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:29:51 +0000

    set difference

---
+++ module_2/module_2_class/class1/25 set difference.py
@@ -0,0 +1,10 @@
+num1 = {1, 2, 3, 4, 5}
+num2 = {5, 6, 7, 8, 9, 10}
+
+# difference_set
+difference_set = num1 - num2
+print('difference_set: ', difference_set)
+
+# difference_set = num1.difference(num2)
+difference_set = num1.difference(num2)
+print('difference_set: ', difference_set)
