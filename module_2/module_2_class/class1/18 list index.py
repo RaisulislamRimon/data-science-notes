94c989d3a1fb8f63366f23638d40eaf1d8704ad8
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 09:16:43 +0000

    list index

---
+++ module_2/module_2_class/class1/18 list index.py
@@ -0,0 +1,17 @@
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+# Find the index of "Charlie" in the list
+charlie_index = names.index("Charlie")
+
+# Print the index
+print(f"The index of Charlie is: {charlie_index}")
+
+
+# bazar list
+bazar_list = list((
+    "chini", "lobon", "ada"
+))
+
+print("Index of lobon: ", bazar_list.index("lobon"))
+print("Index of ada: ", bazar_list.index("ada"))
+print("Index of chini: ", bazar_list.index("chini"))
