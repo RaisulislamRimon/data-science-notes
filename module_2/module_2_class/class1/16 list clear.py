9ffa8b190b6912db0567945de8134cb578fc9da5
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 09:03:31 +0000

    clear function

---
+++ module_2/module_2_class/class1/16 list clear.py
@@ -0,0 +1,11 @@
+# clear function will remove every item from the list
+# 
+
+bazar_list = list((
+    "chini", "lobon", "ada"
+))
+
+print("bazar_list: ", bazar_list)
+
+bazar_list.clear()
+print("bazar list after using clear function: ", bazar_list)
