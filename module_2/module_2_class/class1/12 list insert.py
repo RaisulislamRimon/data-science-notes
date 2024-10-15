3c7688558f747c28d02afd09f935439f836931c4
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 08:14:12 +0000

    insert function

--- module_2/module_2_class/class1/12 list insert.py
+++ module_2/module_2_class/class1/12 list insert.py
@@ -1,3 +1,6 @@
+# insert() function takes two arguments, 
+# the first one is index, and the second one is value
+
 bazar_list = [
     'lebu',
     'chini',
@@ -5,6 +8,4 @@ bazar_list = [
 ]
 
 bazar_list.insert(1, 'pejyaj')  
-# insert() function takes two arguments, 
-# the first one is index, and the second one is value
 print(bazar_list)
