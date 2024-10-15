40970545601d185ce061fd2acb7c00b9eedfc51f
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:28:06 +0000

    set union

---
+++ module_2/module_2_class/class1/23 set union.py
@@ -0,0 +1,10 @@
+num1 = {1, 2, 3, 4, 5}
+num2 = {5, 6, 7, 8, 9, 10}
+
+# union set
+union_set = num1 | num2 
+print('union_set: ', union_set)
+
+# union_set = num1.union(num2)
+union_set = num1.union(num2)
+print('union_set: ', union_set)
