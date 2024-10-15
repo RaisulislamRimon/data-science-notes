3ab8239560a5f627003e06d7eed9009a329e32df
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:34:10 +0000

    iterating items with for loop

---
+++ module_2/module_2_class/class1/26 iterating items with for loop.py
@@ -0,0 +1,9 @@
+fruits = list((
+    "apple", "banana", "cherry", "strawberry"
+))
+print("fruits: ", fruits)
+print("type of fruits: ", type(fruits))
+
+# iterating through a list using a for loop
+for fruit in fruits:
+    print("fruit: ", fruit)
