c02569c3c9f155396eef55400efefd52cb3193b9
rimonppip0 <rimonppip0@gmail.com>
2024-10-15 12:29:03 +0000

    set intersection

---
+++ module_2/module_2_class/class1/24 set intersection.py
@@ -0,0 +1,10 @@
+num1 = {1, 2, 3, 4, 5}
+num2 = {5, 6, 7, 8, 9, 10}
+
+# intersection_set
+intersection_set = num1 & num2
+print('intersection_set: ', intersection_set)
+
+# intersection_set = num1.intersection(num2)
+intersection_set = num1.intersection(num2)
+print('intersection_set: ', intersection_set)
