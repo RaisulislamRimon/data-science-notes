70101d2c161be6135e09d4c66406702d3570d91c
rimonppip0 <rimonppip0@gmail.com>
2024-10-16 08:45:26 +0000

    Show the odd numbers between 1 to 10 using while loop

--- module_2/module_2_class/class1/32 range odd numbers.py
+++ module_2/module_2_class/class1/32 range odd numbers.py
@@ -1,3 +1,9 @@
 print("Show the odd numbers between 1 to 10 using range function")
 for i in range(1, 10, 2):
     print("Number: ", i)
+
+print("Show the odd numbers between 1 to 10 using while loop")
+i = 1
+while i < 10:
+    print("Number: ", i)
+    i += 2
