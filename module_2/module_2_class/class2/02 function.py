447eb408f890c26edf53dbebecb8b9e4a282e7f4
rimonppip0 <rimonppip0@gmail.com>
2024-10-22 11:39:52 +0000

    function paramater & argument

--- module_2/module_2_class/class2/02 function.py
+++ module_2/module_2_class/class2/02 function.py
@@ -1,4 +1,17 @@
-def Add():
-    print(10 + 20)
+# def Add():
+#     print(10 + 20)
 
-Add()
+# Add()
+
+def Add(x, y):   # x, y are parameters
+    print(x + y)
+
+Add(20, 30) 
+Add(x=10, y=20) # 10, 20, 20, 30 are arguments
+
+a = 13 
+b = 5 
+
+Add(a, b) 
+
+
