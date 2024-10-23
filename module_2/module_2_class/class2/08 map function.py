77b8372c4a3c3a49835153aab0b69b6a5182ad86
rimonppip0 <rimonppip0@gmail.com>
2024-10-23 12:11:25 +0000

    map function update

--- module_2/module_2_class/class2/08 map function.py
+++ module_2/module_2_class/class2/08 map function.py
@@ -10,3 +10,22 @@ print(name)
 x = map(myfunc, ('apple', 'banana', 'cherry'))
 print(x)
 print(list(x))
+
+
+
+def square(x):
+    return x * x
+
+y = map(square, (1, 2, 3, 4))
+print(list(y))
+
+
+input_text = input("Enter: ")
+num = input_text.split()
+
+num2 = []
+for a in num:
+    num2.append(int(a))
+
+result = list(map(square, num2))
+print(result)
