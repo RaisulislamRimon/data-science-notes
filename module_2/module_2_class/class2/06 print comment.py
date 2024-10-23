6d1408e891bd39dd975257d6e48fe5218d477044
rimonppip0 <rimonppip0@gmail.com>
2024-10-23 11:51:03 +0000

    print comment from inside a function

---
+++ module_2/module_2_class/class2/06 print comment.py
@@ -0,0 +1,12 @@
+def just_show():
+    """
+    this program will show the comment only.
+    this is the multi-line comment
+    """
+
+    print("Just_show function")
+
+print(just_show.__doc__)
+
+# this is the single line comment
+
