005095ba40df7a32f99427d2f41d0969937bca3b
rimonppip0 <rimonppip0@gmail.com>
2024-10-23 11:38:10 +0000

    using import to use function from another file

---
+++ module_2/module_2_class/class2/04 import using in function.py
@@ -0,0 +1,10 @@
+import test
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+result = test.Add(a, b)
+
+# print("Result: ", result)
+
+print("Result: ", result)
---
+++ module_2/module_2_class/class2/test.py
@@ -0,0 +1,8 @@
+def Add(x, y):
+    return x + y
+
+# x = 10
+# y = 20
+# total = Add(x, y)
+
+# print(total)
