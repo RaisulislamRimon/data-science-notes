55c9c0f0c07f1b94bb7a8cafa30cf5e9975327cf
rimonppip0 <rimonppip0@gmail.com>
2024-10-23 12:41:46 +0000

    test program

---
+++ module_2/module_2_quiz/test_5 .py
@@ -0,0 +1,33 @@
+# # salary = 8000
+
+# # def printSalary():
+
+# #     salary = 12000
+
+# #     print("Salary:", salary)
+
+# # printSalary()
+
+# # print("Salary:", salary)
+
+
+
+# def fun1(name, age=20):
+
+#          print(name, age)
+
+# fun1('Emma')
+
+# # What is the output of the following list assignment?
+
+# aList = [4, 8, 12, 16]
+
+# aList[1:4] = [20, 24, 28]
+
+# print(aList)
+
+
+# What is the output of the following for loop and range() function
+
+# for num in range(-2,-5,-1):
+#     print(num, end=", ")
