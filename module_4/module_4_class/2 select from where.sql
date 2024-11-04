b97421b54c8c00eb0d65e659c59da200182a6413
rimonppip0 <rimonppip0@gmail.com>
2024-11-04 09:49:33 +0000

    and

--- module_4/module_4_class/2 select from where.sql
+++ module_4/module_4_class/2 select from where.sql
@@ -7,15 +7,16 @@ CREATE TABLE IF NOT EXISTS employees (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(100),
-  	state VARCHAR(100)
+  	state VARCHAR(100),
+    city VARCHAR(100)
 );
 
 -- Inserting data into the table
-INSERT INTO employees (employee_id, first_name, last_name, email, state)
+INSERT INTO employees (employee_id, first_name, last_name, email, state, city)
 VALUES 
-    (1, 'John', 'Doe', 'james.madison@hi.com', 'NY'),
-    (2, 'Jane', 'Smith', 'john.mclean@hi.com', 'NY'),
-    (3, 'Bob', 'Johnson', 'james.buchanan@hi.com', 'US');
+    (1, 'John', 'Doe', 'james.madison@hi.com', 'NY', 'Fairport'),
+    (2, 'Jane', 'Smith', 'john.mclean@hi.com', 'NY', 'New York'),
+    (3, 'Bob', 'Johnson', 'james.buchanan@hi.com', 'US', 'New York');
 
 -- Selecting all the data from the table
 -- SELECT first_name FROM employees;
@@ -29,3 +30,6 @@ SELECT * FROM employees WHERE state = 'NY';
 
 -- select all the employees from US
 SELECT * FROM employees WHERE state = 'US';
+
+-- select all the employees from NY and city fair port
+SELECT * FROM employees WHERE state = 'NY' AND city = 'Fairport';
