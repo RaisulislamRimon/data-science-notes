3d9cf355444d9e8378e298fc74f91132d01ded27
rimonppip0 <rimonppip0@gmail.com>
2024-11-04 09:40:36 +0000

    select from where

---
+++ module_4/module_4_class/2 select from where.sql
@@ -0,0 +1,31 @@
+-- Dropping the table if it already exists to avoid conflicts
+DROP TABLE IF EXISTS employees;
+
+-- Creating the table if it does not exist
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+  	state VARCHAR(100)
+);
+
+-- Inserting data into the table
+INSERT INTO employees (employee_id, first_name, last_name, email, state)
+VALUES 
+    (1, 'John', 'Doe', 'james.madison@hi.com', 'NY'),
+    (2, 'Jane', 'Smith', 'john.mclean@hi.com', 'NY'),
+    (3, 'Bob', 'Johnson', 'james.buchanan@hi.com', 'US');
+
+-- Selecting all the data from the table
+-- SELECT first_name FROM employees;
+-- SELECT * FROM employees;
+-- SELECT last_name FROM employees;
+-- SELECT * FROM employees ORDER BY first_name DESC;
+-- SELECT * FROM employees;
+
+-- select all the employees from NY
+SELECT * FROM employees WHERE state = 'NY';
+
+-- select all the employees from US
+SELECT * FROM employees WHERE state = 'US';
