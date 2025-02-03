3abd45c850674fb123c4e9339532c78676379561
rimonppip0 <rimonppip0@gmail.com>
2025-02-03 10:06:57 +0000

    added module_1 resources

---
+++ 1_module/module_1_class/resources/num_swap.py
@@ -0,0 +1,14 @@
+# Take two number
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+# Swap Them
+"""
+c = a
+a = b
+b = c
+"""
+a, b = b, a
+
+# Show Result
+print(a, b)
---
+++ 1_module/module_1_class/resources/sum_of_nums.py
@@ -0,0 +1,9 @@
+# Take two number as input
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+# Add those number and store it in result
+result = int(a) + int(b)
+
+# Show the result
+print(result)
